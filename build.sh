94460be6ba5c3c75f852988be4654aee03720552
Ryan Clarke <ryan@ryanclarke.net>
2016-02-07 17:01:34 -0500

    Tweak configuration

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 outputdir=public-site
-app=hugo_565cefb.exe
+app=hugo_15.exe
 
 find . -iname "Thumbs.db" | xargs rm
-(cd $outputdir; find * \( ! -path ".git/*" ! -path ".git" \) | xargs rm -rf) 
+(cd $outputdir; find * \( ! -path ".git/*" ! -path ".git" \) | xargs rm -rf --) 
 ./$app -D $@
 ./$app $@
 cp $outputdir/index.xml $outputdir/all.xml
